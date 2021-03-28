15bec4195c362b378cff067c198b4b758e2288bd
egperience <68511462+egperience@users.noreply.github.com>
2021-03-28 16:32:01 +0700

    Update KartuMagicWarnaPelangi.java

--- KartuMagicWarnaPelangi.java
+++ KartuMagicWarnaPelangi.java
@@ -21,11 +21,11 @@ public class KartuMagicWarnaPelangi extends Unocard {
         return face;
     }
     //Setter
-    public void setValue(String value){
-        this.face = value;
+    public void setValue(int value){
+        super.setValue(int value);
     }
     public void setColor(String color){
-        this.face = color;
+        super.setColor(String color);
     }
     public void setFace(String face){
         this.face = face;
