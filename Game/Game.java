030736d08d811c17f3b24a9f32d9bbaa69c65b6e
Ichsandro <60038230+Ichsandro@users.noreply.github.com>
2021-03-29 12:46:55 +0700

    Update Game.java
    add discard, isgameover

--- Game/Game.java
+++ Game/Game.java
@@ -55,4 +55,122 @@ public class Game {
 
         stockPile.add(kartuAwal);
     }   
+    
+    public void isGameOver(){
+        for (String player : this.playerIds) {
+            if (getPlayerDeck(player).isEmpty()){
+                return true;
+            }
+        }
+        return false
+    }
+
+    public boolean validCardPlay(Card card) {
+        return (card.getcolor() == warnaKartu || card.getColor() == angkaKartu);
+    }
+
+    public void checkPlayerTurn(String pid) throws InvalidPlayerTurnException {
+        if(this.playerIds[this.currentPlayer] != pid) {
+            throw new InvalidPlayerTurnException("It's not"+ pid + "'s turn", pid);
+        }
+    }
+    
+    public void discard (String pid, Card card, String declaredColor)
+        throws InvalidColorSubmissionException, InvalidPlayerTurnException, InvalidValueSubmissionException {
+            checkPlayerTurn(pid);
+
+            ArrayList<Card> pDeck = getPlayerDeck(pid);
+
+            if (!validCardPlay(card)) {
+                if(card instanceof Wild){
+                    warnaKartu = card.getColor();
+                    angkaKartu = card.getNumber();
+                }
+
+                if (card.getColor() != warnaKartu) {
+                    String message = ("Invalid player move, expected color"+ warnaKartu +" but got color " + card.getColor());
+                    throw new InvalidColorSubmissionException(message, card.getColor(), warnaKartu);
+                }
+                else if (card.getValue() != angkaKartu){
+                    String message2 = ("Invalid player move, expected value"+ validValue +" but got value " + card.getValue());
+                    throw new InvalidColorSubmissionException(message2, card.getNumber(), angkaKartu);
+                }
+            }
+
+            pDeck.remove(card);
+
+            if ((this.playerIds[currentPlayer]).isEmpty()){
+                String message = new String(this.playerIds[currentPlayer]+ "WON");
+                //END ??
+            }
+
+            warnaKartu = card.getColor();
+            angkaKartu = card.getValue();
+            stockPile.add(card);
+
+            if (GameDirection == false) {
+                currentPlayer = (currentPlayer + 1) % playerIds.length;
+            }
+
+            else if (GameDirection == true) {
+                currentPlayer = (checkPlayerTurn - 1) %playerIds.length;
+                if (currentPlayer == -1) {]
+                    currentPlayer = playerIds.length - 1;
+                }
+            }
+
+            if (card instanceof Wild) {
+                warnaKartu = declaredColor;
+            }
+
+            if (card instanceof DrawTwo) {
+                pid = playerIds[currentPlayer];
+                getPlayerDeck(pid).add(deck.drawCard());
+                getPlayerDeck(pid).add(deck.drawCard());
+                System.out.println("draw 2 cards!");
+            }
+
+            if (card instanceof DrawFour) {
+                pid = playerIds[currentPlayer];
+                getPlayerDeck(pid).add(deck.drawCard());
+                getPlayerDeck(pid).add(deck.drawCard());
+                getPlayerDeck(pid).add(deck.drawCard());
+                getPlayerDeck(pid).add(deck.drawCard());
+                System.out.println("draw 2 cards!");
+            }
+
+            if (card instanceof Skip){
+                System.out.println(playerIds[currentPlayer] + "was skipped");
+                if (GameDirection == false) {
+                    currentPlayer = (currentPlayer + 1) % playerIds.length;
+                }
+    
+                else if (GameDirection == true) {
+                    currentPlayer = (checkPlayerTurn - 1) %playerIds.length;
+                    if (currentPlayer == -1) {]
+                        currentPlayer = playerIds.length - 1;
+                    }
+                }
+            }
+
+            if (card instanceof Reverse){
+                System.out.println(playerIds[currentPlayer] + "change the game direction");
+                GameDirection ^= true;
+                
+                if (GameDirection == false) {
+                    currentPlayer = (currentPlayer + 2) % playerIds.length;
+                }
+    
+                else if (GameDirection == true) {
+                    currentPlayer = (checkPlayerTurn - 2) %playerIds.length;
+                    if (currentPlayer == -1) {]
+                        currentPlayer = playerIds.length - 1;
+                    }
+                    if (currentPlayer == -2) {]
+                        currentPlayer = playerIds.length - 2;
+                    }
+                }
+            }
+
+    }
 }
